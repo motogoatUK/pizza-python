ce13bbd1da5b966f245eb931e656d7254967af7d
motogoatUK <software@motogoat.co.uk>
2025-10-05 22:30:42 +0100

    Add login/logout functionality

--- pizza_python/settings.py
+++ pizza_python/settings.py
@@ -125,6 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
